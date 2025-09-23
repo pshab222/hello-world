b20444ea5f15064bb70da168485be3367735cf38
Payam Shabaneh <pshab22@gmail.com>
2025-09-23 14:48:03 -0500

    starter react module

---
+++ reactive/src/main/java/fep/reactive/portlet/ReactivePortlet.java
@@ -0,0 +1,34 @@
+package fep.reactive.portlet;
+
+import fep.reactive.constants.ReactivePortletKeys;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import java.io.IOException;
+
+import javax.portlet.Portlet;
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author payam
+ */
+@Component(
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.header-portlet-css=/css/index.css",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=" + ReactivePortletKeys.Reactive,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class ReactivePortlet extends MVCPortlet {
+}
